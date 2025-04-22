51a4b0c00e0bf5c8455ce442af8889c91a773644
Juliomundoweb <julio@mundoweb.pe>
2025-04-21 19:24:35 -0500

    add image a customers in migrations

--- database/migrations/2025_04_01_052825_create_customers_table.php
+++ database/migrations/2025_04_01_052825_create_customers_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('customers', function (Blueprint $table) {
             $table->id(); // id int auto_increment primary key
             $table->string('name', 255);
+            $table->string('image', 255)->nullable();
             $table->string('description', 255)->nullable();
             $table->string('status', 255)->nullable();
             $table->timestamps();
